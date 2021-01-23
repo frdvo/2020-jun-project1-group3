08146b5c306aa9cca43968a986a27ff694d599cf
Fer <fernandorrb@gmail.com>
2021-01-24 01:00:05 +1030

    Add backend and provider, Fix Makefile JQ output

--- terraform/_backend.tf
+++ terraform/_backend.tf
@@ -1,6 +1,3 @@
-
-
-
 terraform {
   backend "s3" {
     key    = "da/project3"
--- terraform/_provider.tf
+++ terraform/_provider.tf
@@ -6,7 +6,6 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region                  = "ap-southeast-2"
   profile                 = "default"
